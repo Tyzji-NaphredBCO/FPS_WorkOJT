8903c251ec7d29977d54914ac5d7ed8c5b1a0cf8
Tyzji-NaphredBCO <pinoy1nash@gmail.com>
2024-03-14 20:51:35 +0800

    Adding Score System FAILED

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,15 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Bullet : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    public Text scoreText;
+    public int score = 0;
+
+    void Start()
     {
-        if(collision.gameObject.CompareTag("Target"))
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+
+        // Check if the collision is with any of the target tags
+        if (other.gameObject.CompareTag("Red") || other.gameObject.CompareTag("Blue") ||
+            other.gameObject.CompareTag("Green") || other.gameObject.CompareTag("Yellow") ||
+            other.gameObject.CompareTag("Violet"))
         {
-            print("Hit " + collision.gameObject.name + " !");
-            Destroy(collision.gameObject);
+            scoreText.text = score.ToString();
+            // Print the object hit
+            print("Hit " + other.gameObject.name + " !");
+            
+            // Increment the score
+            score += 1;
+            
+            // Log the score increase
+            Debug.Log("+1 point! New score: " + score);
         }
     }
 }
